5249e3b0bcd82786ecedb04cf4bf2a15686203a1
Mert Yüksekgönül <merdo@inside-65-66-37.wireless.utoronto.ca>
2018-09-17 23:13:00 -0400

    additional work

--- greedy.py
+++ greedy.py
@@ -16,3 +16,41 @@ def interval_scheduler(eventlist):
     return execution
 
 
+#here begins huffman tree implementation
+
+import heapq
+
+class BTNode:
+    def __init__(self,val,let):
+        self.val = val
+        self.letter = let
+        self.left = None
+        self.right = None
+
+#assuming pairs consist of (frequency,letter) tuples.
+testinput = [(5,"a"),(9,"b"),(12,"c"),(13,"d"),(16,"e"),(45,"f")]
+
+def print_tree(root):
+    if root:
+        print(str(root.val)+" "+root.letter)
+        print_tree(root.left)
+        print_tree(root.right)
+
+def huffman_tree(pairs):
+    for i in range(len(pairs)):
+        pairs[i] = (pairs[i][0],BTNode(pairs[i][0],pairs[i][1]))
+    heapq.heapify(pairs)
+    while(len(pairs)>1):
+        freq1, node1 = heapq.heappop(pairs)
+        freq2, node2 = heapq.heappop(pairs)
+        parent =  BTNode(freq1+freq2,"")
+        parent.left = node1
+        parent.right = node2
+        heapq.heappush(pairs,(freq1+freq2,parent))
+    return pairs[0][1]
+
+#insert encoding algorithm using the huffmantree
+def encoding_tree(root,dict(),):
+    return encode(root,dict())
+        
+
